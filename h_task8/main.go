3890449a4d213ccf26e09ef653542f626d22e5d9
Alexander Goncharov <lezzercringe@gmail.com>
2025-08-12 22:56:01 +0300

    add task 8 run example

--- h_task8/main.go
+++ h_task8/main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func btoi(value bool) int {
 	if value {
 		return 1
@@ -11,4 +16,22 @@ func btoi(value bool) int {
 func setNthBit(num *int, ix int, value bool) {
 	bitmask := 1 << ix
 	*num = (*num &^ bitmask) | (-btoi(value) & bitmask)
+}
+
+func main() {
+	i := 0
+	setNthBit(&i, 0, true)
+	setNthBit(&i, 2, true)
+	if i != 5 {
+		fmt.Println("FAILURE")
+		os.Exit(1)
+	}
+
+	setNthBit(&i, 0, false)
+	if i != 4 {
+		fmt.Println("FAILURE")
+		os.Exit(1)
+	}
+
+	fmt.Println("SUCCESS")
 }
